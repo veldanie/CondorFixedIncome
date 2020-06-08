3bdf15bae4dbb5a26b3628dc7e98d1282046b3ae
Daniel Velasquez Vergara <daniel.velasquez@sura-am.com>
2020-06-08 11:30:50 -0500

    par rate index

---
+++ R/par_rate_index.R
@@ -0,0 +1,40 @@
+par_rate_index <- function(par_rate_hist, index_ini = 1000, target_mat = 1, bond_freq = 2, base = 365, slippage = 5){
+
+  rate_dates <- index(par_rate_hist)
+  ini_date <- rate_dates[1]
+  last_date <- tail(rate_dates, 1)
+
+  per <- 1/bond_freq
+  cpn_days <- round(seq(per, target_mat, per) * 365)
+  cpn_times <- cpn_days/base
+  ldates <- length(rate_dates)
+  index_val <- rep(0, ldates)
+  index_val[1] <- index_ini
+
+  par_rate <- as.numeric(par_rate_hist[1])
+  mac_dur <- rep(0, ldates)
+
+  for(i in 2:ldates){
+    delta_days <- as.numeric(rate_dates[i] - rate_dates[i-1])
+    cpn_daysi <- cpn_days - delta_days
+
+    cpn_timesi <- cpn_daysi/base
+    par_ratei <- as.numeric(par_rate_hist[i])
+    lcpn <- length(cpn_times)
+    cf <- rep(par_rate, lcpn) * c(cpn_times[1], diff(cpn_times))
+    cf[lcpn] <- cf[lcpn] + 1
+    df <- 1/((1 + par_ratei) ** cpn_timesi)
+    bond_pr <- sum(cf * df)
+
+    bond_exec_pr <- bond_pr + bond_pr * slippage/10000
+    tc <- index_val[i-1] * (bond_exec_pr - bond_pr)
+    index_val[i] <- index_val[i-1] * bond_pr - tc
+
+    tdf <- cpn_timesi * df
+    mac_dur[i] <- round(-sum(cf * tdf)/bond_pr, 3)
+    par_rate <- par_ratei
+  }
+  index_series <- xts(index_val, order.by = rate_dates)
+  mac_dur <- xts(mac_dur, order.by = rate_dates)
+  return(list(index_series=index_series, mac_dur=mac_dur[-1]))
+}
